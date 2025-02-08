01bd2e7124728d22fd7ac5308541af7cad99b7c0
favtuts <favtuts@gmail.com>
2025-02-08 20:27:27 +0900

    define another variable of string

--- VB.NET8-Begginers/VariableUnderstandingSolution/VariableUnderstanding/Form1.vb
+++ VB.NET8-Begginers/VariableUnderstandingSolution/VariableUnderstanding/Form1.vb
@@ -4,8 +4,14 @@
     Private Sub BtnVariable_Click(sender As Object, e As EventArgs) Handles BtnVariable.Click
 
         Dim FirstName As String
+        Dim LastName As String
         FirstName = "Shane"
-        MessageBox.Show("Good morning " & FirstName & ". How are you?")
+        LastName = "Van Tran"
+        MessageBox.Show("Good morning " & FirstName & " " & LastName & ". How are you?")
+
+        FirstName = "Ronald"
+        LastName = "O'Sullivan"
+        MessageBox.Show("Good morning " & FirstName & " " & LastName & ". How are you?")
 
     End Sub
 End Class
